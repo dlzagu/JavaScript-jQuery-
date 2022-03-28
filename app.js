e46c8666d7a1458638b5514bf2d4586778103bb7
dlzagu <eodnsdlekd@naver.com>
2022-03-29 00:12:42 +0900

    20220329:2-1 과제

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ $('#close').on('click', function(){
  }
 
  document.querySelector('.navbar-toggler').addEventListener('click', show);
+
+ $('form').on('submit', function(e){
+    e.preventDefault();
+    if (document.querySelector('#id-input').value == ""){
+        alert('아이디를 입력하시오');
+        
+    }
+    if (document.querySelector('#password-input').value == "") {
+        alert('비밀번호를 입력하시오');
+    }else if(document.querySelector('#password-input').value.length < 6){
+        alert('6자 이상의 비밀번호를 입력하세요');
+    }
+});
